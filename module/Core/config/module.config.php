345dbee610aa09afb23c56cc428b18a9f111d3ae
Diogo Alves <Diogo2560@gmail.com>
2021-08-25 23:52:41 -0300

    FormElementErrorsFactory

--- module/Core/config/module.config.php
+++ module/Core/config/module.config.php
@@ -1,11 +1,18 @@
 <?php
 
+use Core\Factories\FormElementErrorsFactory;
 use Core\Factories\TransportSmtpFactory;
+use Laminas\Form\View\Helper\FormElementErrors;
 
 return [
     'service_manager' => [
         'factories' => [
             'core.transport.smtp' => TransportSmtpFactory::class
+        ]
+    ],
+    'view_helpers' => [
+        'factories' => [
+            FormElementErrors::class => FormElementErrorsFactory::class    
         ]
     ]
 ];
